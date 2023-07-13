56eddcbe35c3ebfdbb4e1180834a96ecb569cdd4
gabrielribery <opportunity.ruoss@gmail.com>
2023-07-13 16:06:51 +0200

    dsfs

--- fragen/q6.php
+++ fragen/q6.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   if (isset($_POST['carbohydrates-meals'])) { // Überprüfen, ob die Frage zu "supplements" beantwortet wurde (aktuelle Frage)
     $_SESSION['activity-importance'] = $_POST['activity-importance'];   // Speichern der vorherigen Benutzerantwort in einer Session-Variablen um das am Ende zusammen zu zählen
     // Weiterleitung zur nächsten Frage
-    header('Location: q3.php'); //Die Funktion "header" ist anscheinend ein vorgegebener Funktionsname in PHP der speziell für das Senden von HTTP-Headern verwendet wird
+    header('Location: q7.php'); //Die Funktion "header" ist anscheinend ein vorgegebener Funktionsname in PHP der speziell für das Senden von HTTP-Headern verwendet wird
     exit;
   }
 }
